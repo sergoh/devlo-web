245a6a301ba1106a0c852758b43aa18387dac4a6
Sergio Lopez <sergio@sergios-mbp.attlocal.net>
2017-03-15 15:17:21 -0700

    init

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('devloWeb')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
